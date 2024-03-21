d7160f3df400bf21e6c59168c4bb110ae17a17e4
HaydarCanDurmaz <candrhydr@hotmail.com>
2024-03-21 16:54:42 +0300

    RequestSpecification   ResponseSpecification

--- src/test/java/Model/ZippoTest.java
+++ src/test/java/Model/ZippoTest.java
@@ -1,6 +1,12 @@
 package Model;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -10,140 +16,164 @@ import org.hamcrest.Matchers.*;
 
 public class ZippoTest {
     @Test
-    public void test() { // https://api.zippopotam.us/us/90210
-        given().when().then();
+    public void test() {
 
+        given()
+                // Hazırlık işlemleri : (token,send body, parametreler)
+
+                .when()
+                // endpoint (url), metodu
+
+                .then()
+        // assertion, test, data işlemleri
+        ;
     }
 
     @Test
     public void statusCodeTest() {
+
         given()
 
                 .when()
-                .get("https://api.zippopotam.us/us/90210")
+                .get("http://api.zippopotam.us/us/90210")
 
                 .then()
-                .log().body() // .log().all() Her şeyi alır
+                .log().body()    // dönen body json datat sı,   log.all()
+                .statusCode(200) // dönüş kodu 200 mü
+        ;
+    }
+
+
+    @Test
+    public void contentTypeTest() {
+
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()    // dönen body json datat sı,   log.all()
+                .statusCode(200) // dönüş kodu 200 mü
+                .contentType(ContentType.JSON) // dönen sonuç JSON mı
+        ;
+    }
+
+    @Test
+    public void checkCountryInResponseBody() {
+
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()    // dönen body json datat sı,   log.all()
+                .statusCode(200) // dönüş kodu 200 mü
+                .body("country", equalTo("United States")) // body nin country değişkeni "United States" eşit Mİ
+
+        // pm.response.json().id -> body.id
+        ;
+    }
+//
+//    PM                            RestAssured
+//    body.country                  body("country")
+//    body.'post code'              body("post code")
+//    body.places[0].'place name'   body("places[0].'place name'")
+//    body.places.'place name'      body("places.'place name'")
+//    bütün place nameleri bir arraylist olarak verir
+//    https://jsonpathfinder.com/
+
+    @Test
+    public void checkstateInResponseBody() {
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                //.log().body()
                 .statusCode(200)
-
-        ;
-    }
-
-    @Test
-    public void contenTypeCodeTest() {
-        given()
-
-                .when()
-                .get("https://api.zippopotam.us/us/90210")
-
-                .then()
-                .log().body() // .log().all() Her şeyi alır
-                .statusCode(200)
-                .contentType(ContentType.JSON) // dönen sonuç JSon mı
-
-        ;
-    }
-
-    @Test
-    public void checkCountryResponsitvBody() {
-        given()
-
-                .when()
-                .get("https://api.zippopotam.us/us/90210")
-
-                .then()
-                .log().body() // .log().all() Her şeyi alır
-                .statusCode(200) // dönüş kodu 200 mü
-                .contentType(ContentType.JSON) // dönen sonuç JSon mı
-                .body("country", equalTo("United States")) // body conutry degişkeni United States eşitmi
-
-        ;
-    }
-
-    @Test
-    public void checkCityInResponsiveBody() {
-        given()
-
-                .when()
-                .get("https://api.zippopotam.us/us/90210")
-
-                .then()
-                .log().body() // .log().all() Her şeyi alır
-                .statusCode(200) // dönüş kodu 200 mü
-                .contentType(ContentType.JSON) // dönen sonuç JSon mı
-                .body("places[0].state", equalTo("California")) // body conutry degişkeni United States eşitmi
-
-        ;
-    }
-
-    @Test
-    public void chackHasItemy() {
-        given()
-
-                .when()
-                .get("https://api.zippopotam.us/tr/01000")
-
-                .then()
-                .log().body() // .log().all() Her şeyi alır
-                .statusCode(200) // dönüş kodu 200 mü
-                .body("places.'place name'", hasItem("Dörtağaç Köyü"))
-
-
-        ;
-    }
-
-    @Test
-    public void BodyArrayHasSizeTest() {
-        given()
-
-                .when()
-                .get("https://api.zippopotam.us/us/90210")
-
-                .then()
-                // .log().body() // .log().all() Her şeyi alır
-                .statusCode(200) // dönüş kodu 200 mü
-                .body("places", hasSize(1))
                 .body("places[0].state", equalTo("California"))
-                .body("places[0].'place name'", equalTo("Beverly Hills"))
-
-
         ;
     }
 
     @Test
-    public void pathparamTest() {
+    public void checkHasItemy() {
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/tr/01000")
+
+                .then()
+                //.log().body()
+                .statusCode(200)
+                .body("places.'place name'", hasItem("Dörtağaç Köyü"))
+        // bütün place name lerin herhangi birinde Dörtağaç Köyü varmı
+        ;
+    }
+
+    @Test
+    public void bodyArrayHasSizeTest() {
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                //.log().body()
+                .statusCode(200)
+                .body("places", hasSize(1))
+        ;
+    }
+
+    @Test
+    public void combiningTest() {
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                //.log().body()
+                .statusCode(200)
+                .body("places", hasSize(1))  // size ı 1 mi
+                .body("places.state", hasItem("California")) // verilen path deki list bu item e sahip mi
+                .body("places[0].'place name'", equalTo("Beverly Hills")) // verilen path deki değer buna eşit mi
+        ;
+    }
+
+
+    @Test
+    public void pathParamTest() {
         given()
                 .pathParam("ulke", "us")
                 .pathParam("postaKod", 90210)
-
+                .log().uri() // request Link
 
                 .when()
-                .get("https://api.zippopotam.us/{ulke}/{postaKod}")
+                .get("http://api.zippopotam.us/{ulke}/{postaKod}")
 
                 .then()
                 .statusCode(200)
         //.log().body()
-
-
         ;
     }
 
     @Test
     public void queryParamTest() {
-        given()
-                .param("page", 1)
-                .log().uri()
+        // https://gorest.co.in/public/v1/users?page=3
 
+        given()
+                .param("page", 1)  // ?page=1  şeklinde linke ekleniyor
+                .log().uri() // request Link
 
                 .when()
-                .get("\n" +
-                        "https://gorest.co.in/public/v1/users")
+                .get("https://gorest.co.in/public/v1/users")  // ?page=1
 
                 .then()
                 .statusCode(200)
                 .log().body()
-
-
         ;
     }
 
@@ -167,5 +197,42 @@ public class ZippoTest {
                     .body("meta.pagination.page", equalTo(i))
             ;
         }
+
+
+    }
+    RequestSpecification requestSpec;
+    ResponseSpecification responseSpec;
+
+    @BeforeClass
+    public void Setup() {
+
+        baseURI = "https://gorest.co.in/public/v1";
+
+        requestSpec = new RequestSpecBuilder()
+                .log(LogDetail.URI)
+                .setContentType(ContentType.JSON)
+                .build();
+
+        responseSpec = new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .expectStatusCode(200)
+                .log(LogDetail.BODY)
+                .build();
+    }
+    @Test
+    public void requestResponseSpecificationn() {
+        // https://gorest.co.in/public/v1/users?page=3
+
+        given()
+                .param("page", 1)  // ?page=1  şeklinde linke ekleniyor
+                .spec(requestSpec)
+
+                .when()
+                .get("/users")  // ?page=1
+
+                .then()
+                .spec(responseSpec)
+        ;
     }
 }
+
