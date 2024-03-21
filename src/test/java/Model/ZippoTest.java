2ae083bb9acbd2f221f37d3aa3aa8f20fda29eae
HaydarCanDurmaz <candrhydr@hotmail.com>
2024-03-21 16:39:04 +0300

    queryParamTest2

--- src/test/java/Model/ZippoTest.java
+++ src/test/java/Model/ZippoTest.java
@@ -146,4 +146,26 @@ public class ZippoTest {
 
         ;
     }
+
+    @Test
+    public void queryParamTest2() {
+        // https://gorest.co.in/public/v1/users?page=3
+        // bu linkteki 1 den 10 kadar sayfaları çağırdığınızda response daki donen page degerlerinin
+        // çağrılan page nosu ile aynı olup olmadığını kontrol ediniz.
+
+        for (int i = 1; i < 10; i++) {
+            given()
+                    .param("page", i)  // ?page=1  şeklinde linke ekleniyor
+                    .log().uri() // request Link
+
+                    .when()
+                    .get("https://gorest.co.in/public/v1/users")  // ?page=1
+
+                    .then()
+                    .statusCode(200)
+                    .log().body()
+                    .body("meta.pagination.page", equalTo(i))
+            ;
+        }
+    }
 }
