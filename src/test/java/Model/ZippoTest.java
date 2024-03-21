19a79b6cdf2fbd715ab629f2c6b89c0617f6ad93
HaydarCanDurmaz <candrhydr@hotmail.com>
2024-03-21 16:23:49 +0300

    pathparamTest

--- src/test/java/Model/ZippoTest.java
+++ src/test/java/Model/ZippoTest.java
@@ -2,20 +2,21 @@ package Model;
 
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
+
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.*;
 
 import org.hamcrest.Matchers.*;
 
 public class ZippoTest {
     @Test
-    public void test(){ // https://api.zippopotam.us/us/90210
+    public void test() { // https://api.zippopotam.us/us/90210
         given().when().then();
 
     }
+
     @Test
-    public void statusCodeTest(){
+    public void statusCodeTest() {
         given()
 
                 .when()
@@ -27,8 +28,9 @@ public class ZippoTest {
 
         ;
     }
+
     @Test
-    public void contenTypeCodeTest(){
+    public void contenTypeCodeTest() {
         given()
 
                 .when()
@@ -41,8 +43,9 @@ public class ZippoTest {
 
         ;
     }
+
     @Test
-    public void checkCountryResponsitvBody(){
+    public void checkCountryResponsitvBody() {
         given()
 
                 .when()
@@ -52,12 +55,13 @@ public class ZippoTest {
                 .log().body() // .log().all() Her şeyi alır
                 .statusCode(200) // dönüş kodu 200 mü
                 .contentType(ContentType.JSON) // dönen sonuç JSon mı
-                .body("country",equalTo("United States")) // body conutry degişkeni United States eşitmi
+                .body("country", equalTo("United States")) // body conutry degişkeni United States eşitmi
 
         ;
     }
+
     @Test
-    public void checkCityInResponsiveBody(){
+    public void checkCityInResponsiveBody() {
         given()
 
                 .when()
@@ -67,12 +71,13 @@ public class ZippoTest {
                 .log().body() // .log().all() Her şeyi alır
                 .statusCode(200) // dönüş kodu 200 mü
                 .contentType(ContentType.JSON) // dönen sonuç JSon mı
-                .body("places[0].state",equalTo("California")) // body conutry degişkeni United States eşitmi
+                .body("places[0].state", equalTo("California")) // body conutry degişkeni United States eşitmi
 
         ;
     }
+
     @Test
-    public void chackHasItemy (){
+    public void chackHasItemy() {
         given()
 
                 .when()
@@ -81,10 +86,45 @@ public class ZippoTest {
                 .then()
                 .log().body() // .log().all() Her şeyi alır
                 .statusCode(200) // dönüş kodu 200 mü
-                .body("places.'place name'",hasItem("Dörtağaç Köyü"))
+                .body("places.'place name'", hasItem("Dörtağaç Köyü"))
+
+
+        ;
+    }
+
+    @Test
+    public void BodyArrayHasSizeTest() {
+        given()
+
+                .when()
+                .get("https://api.zippopotam.us/us/90210")
+
+                .then()
+                // .log().body() // .log().all() Her şeyi alır
+                .statusCode(200) // dönüş kodu 200 mü
+                .body("places", hasSize(1))
+                .body("places[0].state", equalTo("California"))
+                .body("places[0].'place name'", equalTo("Beverly Hills"))
+
+
+        ;
+    }
+
+    @Test
+    public void pathparamTest() {
+        given()
+                .pathParam("ulke", "us")
+                .pathParam("postaKod", 90210)
+
+
+                .when()
+                .get("https://api.zippopotam.us/{ulke}/{postaKod}")
+
+                .then()
+                .statusCode(200)
+                //.log().body()
 
 
         ;
     }
 }
-//
